cece6a98dbe4f31cc1d5c144aa0c479e00172713
dego <dcordova7146@bths.edu>
2018-09-18 10:14:40 -0400

    Day 1 -one method finished

--- src/DCordova/DCordovaLib.java
+++ src/DCordova/DCordovaLib.java
@@ -6,4 +6,22 @@ public class DCordovaLib {
     {
         System.out.println(str);
     }
+
+    public static boolean isPalindrome(String phrase){ //Working
+        String reversed = "";
+
+        for(int i = phrase.length()-1; i >= 0; i-- ){
+            reversed += phrase.substring(i, i+1);
+        }
+
+        return (reversed.equals(phrase));
+    }
+
+    public static String dateStr(int month, int day, int year){
+
+    }
+
+    public static String cutOut(String main, String cut){
+        
+    }
 }
--- src/DCordova/TestFile.java
+++ src/DCordova/TestFile.java
@@ -1,12 +1,12 @@
 package DCordova;
 
-import static DCordova.DCordovaLib.println;
+import static DCordova.DCordovaLib.isPalindrome;
 
 public class TestFile {
 
         public static void main(String[] args)
         {
-            println("I love a cup of java in the morning.");
+            System.out.println(isPalindrome("racecar"));
         }
 }
 
