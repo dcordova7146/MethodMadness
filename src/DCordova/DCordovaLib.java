74580a12fc78273e19cee3b4e39da13ca19a8932
dego <cdemian576@bths.edu>
2018-09-20 10:18:16 -0400

    Day 2

--- src/DCordova/DCordovaLib.java
+++ src/DCordova/DCordovaLib.java
@@ -1,3 +1,5 @@
+//Diego Cordova 9/20/2018;
+
 package DCordova;
 
 public class DCordovaLib {
@@ -17,11 +19,37 @@ public class DCordovaLib {
         return (reversed.equals(phrase));
     }
 
-    public static String dateStr(int month, int day, int year){
+   //public static String dateStr(int month, int day, int year){
+
+
+    //}
+
+    public static String cutOut(String main, String cut){ //Working
+        String part1 = "";
+        String part2 = "";
+        if(main.indexOf(cut) < 0) {
+           return(main);
+        }
+        //main.substring(main.indexOf(cut),main.indexOf(cut)+cut.length())
+        part1 = main.substring(0, main.indexOf(cut));
+        part2 = main.substring(main.indexOf(cut) + cut.length());
+        return (part1 + part2);
 
     }
 
-    public static String cutOut(String main, String cut){
-        
+    public static int sumUpTo(int integer){
+        int x = 0;
+        if(integer == 0) {
+            return 0;
+        }
+        x = ((integer/2)*(1+integer));
+
+        return x;
     }
+
+    public static primePrinter(int num){
+
+    }
+
+
 }
--- src/DCordova/TestFile.java
+++ src/DCordova/TestFile.java
@@ -1,13 +1,21 @@
 package DCordova;
 
+import static DCordova.DCordovaLib.cutOut;
 import static DCordova.DCordovaLib.isPalindrome;
+import static DCordova.DCordovaLib.sumUpTo;
 
 public class TestFile {
 
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
             System.out.println(isPalindrome("racecar"));
+            System.out.println(isPalindrome("bottle"));
+            System.out.println(cutOut("catattack", "att"));
+            System.out.println(sumUpTo(2));
+            System.out.println(sumUpTo(100));
+            System.out.println(sumUpTo(  0));
         }
+
+
 }
 
 
