d78884bcda3e504281312abc98c144ed81183527
Dego <dcordova7146@bths.ed>
2018-09-21 10:19:25 -0400

    Day 3 affter peer review

--- src/DCordova/DCordovaLib.java
+++ src/DCordova/DCordovaLib.java
@@ -9,6 +9,11 @@ public class DCordovaLib {
         System.out.println(str);
     }
 
+    /**
+     *checks if a phrase is a palindrome
+     * @param phrase a phrase to be checked for if it is a palindrome which is a word that is the same spelled backwords
+     * @return boolean: if it is a palindrome it shall return true
+     */
     public static boolean isPalindrome(String phrase){ //Working
         String reversed = "";
 
@@ -24,6 +29,12 @@ public class DCordovaLib {
 
     //}
 
+    /**
+     *cuts out a desired word out of a bigger word
+     * @param main a word or phrase
+     * @param cut  a portion of the main that you wish to be taken out of the word
+     * @return a string that is the main word phrase without the "cut" portion
+     */
     public static String cutOut(String main, String cut){ //Working
         String part1 = "";
         String part2 = "";
@@ -37,7 +48,12 @@ public class DCordovaLib {
 
     }
 
-    public static int sumUpTo(int integer){
+    /**
+     *adds up all number within a givin number
+      * @param integer a number which you want the sum of all its previous numbers
+     * @return and integer that is the sum of all the previous numbers inclusive
+     */
+    public static int sumUpTo(int integer){  //working
         int x = 0;
         if(integer == 0) {
             return 0;
@@ -47,8 +63,38 @@ public class DCordovaLib {
         return x;
     }
 
-    public static primePrinter(int num){
+    /**
+     *
+     * @param num the upper limit for how many prime numbers you wish printed
+     * @return returning a list of the first prime numbers up until num
+     */
+    /**
+    public static void primePrinter(int num){
+        String list = "";
+        for(int i = 1;i != num; i ++){
+            boolean isprime = checkprime(i);
+            if(isprime == true){
+                list = integer.parseInt(i)
+            }
+        }
 
+    }
+
+    /**
+     *
+     * @param num
+     * @return
+     */
+    public static boolean checkprime(int num){
+        if( num < 2){
+            return false;
+        }
+        for(int i = 2;i < num;i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 
 
--- src/DCordova/TestFile.java
+++ src/DCordova/TestFile.java
@@ -1,18 +1,19 @@
 package DCordova;
 
-import static DCordova.DCordovaLib.cutOut;
-import static DCordova.DCordovaLib.isPalindrome;
-import static DCordova.DCordovaLib.sumUpTo;
+import static DCordova.DCordovaLib.*;
 
 public class TestFile {
 
         public static void main(String[] args) {
+
             System.out.println(isPalindrome("racecar"));
             System.out.println(isPalindrome("bottle"));
             System.out.println(cutOut("catattack", "att"));
+            System.out.println(cutOut("catcatcat", "cat"));
             System.out.println(sumUpTo(2));
             System.out.println(sumUpTo(100));
             System.out.println(sumUpTo(  0));
+            //System.out.println(checkprime(  3));
         }
 
 
